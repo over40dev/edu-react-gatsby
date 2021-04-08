2d5aed0fe213bf7e24d256d59eb54416b487d633
over40dev <ted@over40.dev>
2021-04-08 21:14:15 -0230

    17-gatsby-plugins

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from './src/components/Layout';
 
-export function wrapPageElement({element}) {
-  return <Layout>{element}</Layout>
+export function wrapPageElement({ element }) {
+  return <Layout>{element}</Layout>;
 }
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
-    title:  `Slicks Slices`,
-    siteUrl: 'https://gatsby.pizza',
+    title: `Slicks Slices`,
+    siteUrl: 'localhost:3333',
     description: 'The Best Pizza',
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'vgus6rh0',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };

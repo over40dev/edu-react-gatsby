9513af6064d92c335574ec776c1f19ab645081ef
over40dev <ted@over40.dev>
2021-04-11 21:24:47 -0230

    23 - Dynamically creating pages | gatsby-node (1)

---
+++ gatsby-node.js
@@ -0,0 +1,44 @@
+import path from 'path';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  // 1. Get a template for this page
+  const pizzaTemplate = path.resolve(`./src/templates/Pizza.js`);
+  //  2. Query all pizzas
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  // console.log(data);
+  //  3. Loop over each pizza and create a page for that pizza
+  // use forEach() because we are just looping... not returning anything
+  data.pizzas.nodes.forEach((pizza) => {
+    // console.log(`Creating page for ${p.name}`);
+    // .createPage() is the actual method while createPages is the Hook into it
+    actions.createPage({
+      // What is the URL for this new page??
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        over40dev: 'is awesome!!',
+        slug: pizza.slug.current,
+      },
+    });
+  });
+}
+
+export async function createPages(params) {
+  // create pages dynamically
+  // 1. Pizzas
+  // use await because we are calling async function
+  await turnPizzasIntoPages(params);
+  //  2. Toppings
+  //  3. Slicemasters
+}

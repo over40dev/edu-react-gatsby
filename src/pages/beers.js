3b16376ac1303220a2a8f3baa81605a183383930
over40dev <ted@over40.dev>
2021-04-15 16:28:17 -0230

    27-Querying, Displaying and Styling Beers Page (1)

--- src/pages/beers.js
+++ src/pages/beers.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function BeersPage() {
+// where do the PROPS come from (not passed directly) - Gatsby magic!!!
+export default function BeersPage({ data, pageContext }) {
+  const allBeers = data.beers.nodes;
+
   return (
-    <>
-      <p>Hey! I'm the beers page!</p>
-    </>
+    <div>
+      {allBeers.map(
+        ({ beer }, idx) => (
+          <div key={idx}>
+            <img src={beer.image} alt="beer" />
+            <h2>
+              <span className="mark">{beer.name}</span>
+              <span>{beer.price}</span>
+            </h2>
+          </div>
+        )
+        // console.log('processing beer: ', beer.name);
+      )}
+    </div>
   );
 }
+
+export const query = graphql`
+  query BeersQuery {
+    beers: allBeer {
+      nodes {
+        beer {
+          name
+          price
+          image
+          rating {
+            average
+            reviews
+          }
+        }
+      }
+    }
+  }
+`;
+// 2. Loop over beers and turn into pages
+// data.beers.nodes.forEach((beer) => {
+//   console.log(beer.name);
+//   // console.log(`Creating page for beer: ${beer.name}`);
+// });

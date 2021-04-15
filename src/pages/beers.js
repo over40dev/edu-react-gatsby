93c697bcbeb61b45b40c75322842700a9c852a94
over40dev <ted@over40.dev>
2021-04-15 20:00:24 -0230

    27-Querying, Displaying and Styling Beers Page (2)

--- src/pages/beers.js
+++ src/pages/beers.js
@@ -1,47 +1,75 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
+
+const BeerGridStyle = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingleBeerStyles = styled.div`
+  border: 10px solid var(--grey);
+  padding: 2rem;
+  text-align: center;
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: contain;
+    display: block;
+    display: grid;
+    align-items: center;
+    font-size: 10px;
+  }
+  /* target reviews (could add class) */
+  span:nth-child(2) {
+    margin-left: 10px;
+  }
+`;
 
 // where do the PROPS come from (not passed directly) - Gatsby magic!!!
-export default function BeersPage({ data, pageContext }) {
+export default function BeersPage({ data }) {
   const allBeers = data.beers.nodes;
 
   return (
-    <div>
-      {allBeers.map(
-        ({ beer }, idx) => (
-          <div key={idx}>
-            <img src={beer.image} alt="beer" />
-            <h2>
-              <span className="mark">{beer.name}</span>
-              <span>{beer.price}</span>
-            </h2>
-          </div>
-        )
-        // console.log('processing beer: ', beer.name);
-      )}
-    </div>
+    <>
+      <h2>We have {allBeers.length} Beers Available. Dine in Only!</h2>
+      <BeerGridStyle>
+        {allBeers.map((beer) => {
+          const rating = Math.round(beer.rating.average);
+          return (
+            <SingleBeerStyles key={beer.id}>
+              <img src={beer.image} alt={beer.name} />
+              <h3>{beer.name}</h3>
+              {beer.price}
+              <p title={`${rating} out of 5 stars`}>
+                {`⭐`.repeat(rating)}
+                <span style={{ filter: `grayscale(100%)` }}>
+                  {`⭐`.repeat(5 - rating)}
+                </span>
+                <span>({beer.rating.reviews})</span>
+              </p>
+            </SingleBeerStyles>
+          );
+        })}
+      </BeerGridStyle>
+    </>
   );
 }
 
 export const query = graphql`
-  query BeersQuery {
+  query {
     beers: allBeer {
       nodes {
-        beer {
-          name
-          price
-          image
-          rating {
-            average
-            reviews
-          }
+        id
+        name
+        price
+        image
+        rating {
+          average
+          reviews
         }
       }
     }
   }
 `;
-// 2. Loop over beers and turn into pages
-// data.beers.nodes.forEach((beer) => {
-//   console.log(beer.name);
-//   // console.log(`Creating page for beer: ${beer.name}`);
-// });

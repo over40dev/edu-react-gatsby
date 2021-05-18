558c619d4434915fab094945cad8b4b76d3e029b
over40dev <ted@over40.dev>
2021-05-17 22:29:00 -0230

    38-Move Order State to Context custom Provider

--- src/utils/usePizza.js
+++ src/utils/usePizza.js
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import OrderContext from '../components/OrderContext';
 
 export default function usePizza({ pizzas, inputs }) {
   // 1. create state to hold order
-  const [order, setOrder] = useState([]);
+  // we get rid of this next line because we moved useState up to the provider
+  // const [order, setOrder] = useState([]);
+  // Now we access both our state and our updater function (setOrder) via context
+  const [order, setOrder] = useContext(OrderContext);
+
   // 2. Make a function to add things to order
   function addToOrder(orderedPizza) {
     setOrder([...order, orderedPizza]);
